786d15c611aabc5e346c64ad9716df3b35cf014a
Your Name <you@example.com>
2023-05-15 18:42:39 +0000

    Authentication-Functionality

---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+// Write your JS code here
+// Write your JS code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <div className="header-container">
+    <ul>
+      <li className="list">
+        <Link to="/" className="text">
+          Home
+        </Link>
+      </li>
+
+      <li className="list">
+        <Link to="/about" className="text">
+          About
+        </Link>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Header
